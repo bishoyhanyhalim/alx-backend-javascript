0ed0d1c84ba7385d77000127ae8df582dd0e097e
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-07-23 18:23:46 -0700

    all files

--- 0x00-ES6_basic/100-createIteratorObject.js
+++ 0x00-ES6_basic/100-createIteratorObject.js
@@ -1,1 +1,8 @@
-#!/usr/bin/node
+export default function createIteratorObject(report) {
+  const myarray = [];
+  for (const item of Object.values(report.allEmployees)) {
+    myarray.push(...item);
+  }
+
+  return myarray;
+}
