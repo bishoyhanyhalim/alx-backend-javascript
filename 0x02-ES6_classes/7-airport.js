67b3c577b3b781f177db3c7b0962a35b0cb41c04
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-07-25 15:59:10 -0700

    task form 5 to 9

--- 0x02-ES6_classes/7-airport.js
+++ 0x02-ES6_classes/7-airport.js
@@ -1,1 +1,26 @@
-#!/usr/bin/node
+export default class Airport {
+  constructor(name, code) {
+    this._name = name;
+    this._code = code;
+  }
+
+  get name() {
+    return this._name;
+  }
+
+  set name(value) {
+    this._name = value;
+  }
+
+  get code() {
+    return this._code;
+  }
+
+  set code(value) {
+    this._code = value;
+  }
+
+  toString() {
+    return `[object ${this.code}]`;
+  }
+}
