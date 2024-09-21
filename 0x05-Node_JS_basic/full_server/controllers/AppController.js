a3175c465647240872de07bcde50d94849b44318
bishoyhanyhalim <bishoyhanyhalim@gmail.com>
2024-09-21 15:28:08 -0700

    task 8

--- 0x05-Node_JS_basic/full_server/controllers/AppController.js
+++ 0x05-Node_JS_basic/full_server/controllers/AppController.js
@@ -1,1 +1,11 @@
-#!/usr/bin/node
+/**
+ * Miscellaneous route for task
+ */
+class AppController {
+  static getHomepage(request, response) {
+    response.status(200).send('Hello Holberton School!');
+  }
+}
+
+export default AppController;
+module.exports = AppController;
